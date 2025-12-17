fb6cba3422e28ba8cabab02cf79d371bd522b895
Simon <shartmann@uni-muenster.de>
2025-12-17 21:16:53 +0100

    fix: enabled cors handling to not fail when preflight requests are sent

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from api.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="TTP v2 Backend",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
