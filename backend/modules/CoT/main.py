0c1e9c3ee84660de6fed12096a70631bb8b9f2a3
Simon <shartmann@uni-muenster.de>
2025-12-18 19:33:32 +0100

    feat: introduce sleep time to simulate workload and test async task

--- backend/modules/CoT/main.py
+++ backend/modules/CoT/main.py
@@ -1,7 +1,13 @@
+from time import sleep
 from openai import OpenAI
 from .prompt import build_prompt
+from datetime import datetime
 
 client = OpenAI()
 
 def run(payload) -> str:
+    ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[CoT][{ts}] started with payload: {payload} ")
+    sleep(5) 
+    print(f"[CoT][{ts}] finished with payload: {payload} ")
     return f"I am running the CoT module! I am carrying a heavy payload: {payload}"
