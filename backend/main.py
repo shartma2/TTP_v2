06d0e56d871004e571fad90cb1f75a0e8c5700f1
Simon <shartmann@uni-muenster.de>
2026-02-23 11:42:03 +0100

    chore: configure different port binding

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ def create_app() -> FastAPI:
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "http://localhost:3000",
+            "http://localhost:3001",
         ],
         allow_credentials=True,
         allow_methods=["*"],
